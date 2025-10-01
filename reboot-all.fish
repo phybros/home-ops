b0f3de762d735ffc060ff2532ec35979253f3dd9
Will Warren <willdavidwarren@gmail.com>
2025-10-01 17:31:18 -0400

    chore(scripts): some updates to the rebooter

--- reboot-all.fish
+++ reboot-all.fish
@@ -1,7 +1,12 @@
 #!/opt/homebrew/bin/fish
 
-for ip in 235 236 237;
-  talosctl -n "192.168.86.$ip" reboot
-  sleep 120
-end
+set ips 235 236 237
 
+for ip in $ips
+  talosctl -n "192.168.86.$ip" reboot
+
+  # Only sleep if not the last IP
+  if test $ip != $ips[-1]
+    sleep 120
+  end
+end
